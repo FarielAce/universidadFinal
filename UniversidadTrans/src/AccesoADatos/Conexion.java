76bc55c3cecb78acaf127d2b080b15b6266764ce
GabyServidio <gabrielserv@hotmail.com.ar>
2023-09-07 12:45:35 -0300

    Se crearon la clase connection

--- UniversidadTrans/src/AccesoADatos/Conexion.java
+++ UniversidadTrans/src/AccesoADatos/Conexion.java
@@ -1,13 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package AccesoADatos;
 
-/**
- *
- * @author USUARIO
- */
+import java.sql.Connection;
+
 public class Conexion {
+    public static Connection connection;
     
 }
