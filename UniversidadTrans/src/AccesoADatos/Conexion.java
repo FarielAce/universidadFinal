e25ac6384e2d8ab336f69c07607e343f7ad4fb25
FarielAce <f.ariel.acevedo@gmail.com>
2023-09-08 10:41:17 -0300

    Update Conexion.java

--- UniversidadTrans/src/AccesoADatos/Conexion.java
+++ UniversidadTrans/src/AccesoADatos/Conexion.java
@@ -1,8 +1,26 @@
 package AccesoADatos;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import javax.swing.JOptionPane;
 
 public class Conexion {
-    public static Connection connection;
+       private static final String URL = "jdbc:mysql://localhost:3306/universidad";
+    private static final String USER = "root";
+    private static final String PASS = "";
+         
+    public static Connection getConexion(){
+        Connection conexion = null;   
+        try {
+            Class.forName("org.mariadb.jdbc.Driver");
+            conexion = DriverManager.getConnection(URL, USER, PASS);
+        } catch (SQLException ex) {
+           JOptionPane.showMessageDialog(null, "ERROR DE CONEXION CON EL SERVIDOR");
+        } catch (ClassNotFoundException ex) {
+            JOptionPane.showMessageDialog(null, "ERROR DE DRIVER");
+        }
+    return conexion;
+    }
     
 }
