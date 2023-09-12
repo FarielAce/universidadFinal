7d1bca65062bdff614897c921f9b32b122e0c3fb
FarielAce <f.ariel.acevedo@gmail.com>
2023-09-12 11:08:10 -0300

    Gestion Alumno create
    interface Grafica de gestion de alumnos

--- UniversidadTrans/src/universidadtrans/UniversidadTrans.java
+++ UniversidadTrans/src/universidadtrans/UniversidadTrans.java
@@ -13,11 +13,11 @@ import Entidades.*;
  * @author Test
  */
 public class UniversidadTrans {
+public static InscripcionData controlInscripcion = new InscripcionData();
+   
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
+public static void main(String[] args) {
+     
      
   
     }
